f367cc9ed1296728d2c097f5e5200c2139838f46
gtasa3box <gtasa3box@gmail.com>
2021-09-06 17:13:35 +0300

    commit

--- src/js/global.js
+++ src/js/global.js
@@ -62,3 +62,40 @@ $('.textile__slider').slick({
    ]
  });
  //end-slider-textile
+
+ //slider-examples
+
+ $('.examples__slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+//end-slider-examples
