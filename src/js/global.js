e62cf31c12efec7f62f1684734a8c028c2acf2ff
gtasa3box <gtasa3box@gmail.com>
2021-09-06 13:03:23 +0300

    commit

--- src/js/global.js
+++ src/js/global.js
@@ -1,1 +1,25 @@
 console.log('global');
+
+$(document).ready(function () {
+    $(".drop-1").hover(
+  function () {
+     $('.dropbox-1').slideDown('medium');
+  }, 
+  function () {
+     $('.dropbox-1').slideUp('medium');
+  }
+  
+);
+});
+
+$(document).ready(function () {
+    $(".drop-2").hover(
+  function () {
+     $('.dropbox-2').slideDown('medium');
+  }, 
+  function () {
+     $('.dropbox-2').slideUp('medium');
+  }
+  
+);
+});
