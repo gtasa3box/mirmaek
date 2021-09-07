d599afb8b4a405002afb3eecce70db3a45a14747
gtasa3box <gtasa3box@gmail.com>
2021-09-07 14:41:08 +0300

    commit

--- src/js/global.js
+++ src/js/global.js
@@ -99,3 +99,61 @@ $('.textile__slider').slick({
 });
 
 //end-slider-examples
+
+//slider-product
+
+$('.slider-product__slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+//end-slider-product
+
+//button-top
+
+$(document).ready(function() { 
+  var button = $('#button-up');	
+  $(window).scroll (function () {
+    if ($(this).scrollTop () > 300) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+});	 
+button.on('click', function(){
+$('body, html').animate({
+scrollTop: 0
+}, 800);
+return false;
+});		 
+});
+
+//end-button-top
