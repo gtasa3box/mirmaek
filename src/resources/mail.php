e2ab9f615da6d4f86ebe0a53187229d39e7d7a29
gtasa3box <gtasa3box@gmail.com>
2021-09-17 16:16:45 +0300

    commit fix

--- src/resources/mail.php
+++ src/resources/mail.php
@@ -14,8 +14,13 @@ if ( $method === 'POST' ) {
 	$city = trim($_POST["city"]);
 	$adres = trim($_POST["adres"]);
 	$product = trim($_POST["product"]);
-	$img = trim($_POST["img"]);
+	$img = trim($_POST["fileToUpload"]);
 
+	$target_dir = __DIR__.'/uploads/';
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	
 	foreach ( $_POST as $key => $value ) {
 		if ( $value != "") {
 			$message .= "
@@ -26,6 +31,42 @@ if ( $method === 'POST' ) {
 			";
 		}
 	}
+
+
+	// Check if image file is a actual image or fake image
+	
+	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+	if($check !== false) {
+		echo "File is an image - " . $check["mime"] . ".";
+		$uploadOk = 1;
+	} else {
+		echo "File is not an image.";
+		$uploadOk = 0;
+	}
+
+	if ($_FILES["fileToUpload"]["size"] > 2000000) {
+		echo "Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+	
+
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		$uploadOk = 0;
+	}
+
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+		echo "Sorry, your file was not uploaded.";
+  	// if everything is ok, try to upload file
+	} else {
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+		} else {
+		echo "Sorry, there was an error uploading your file.";
+		}
+	}
+
 }
 
 $message = "<table style='width: 100%;'>$message</table>";
